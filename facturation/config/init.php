833b55850776bbd4f1b51f1ee18b1772fb53889b
Chapeau Gris <lionel@chapeau-gris.fr>
2014-03-28 15:47:47 +0100

    remove uneeded init var

--- facturation/config/init.php
+++ facturation/config/init.php
@@ -1,16 +1,3 @@
-<?
-$_dbname = '' ;
-$_dbuser = '' ;
-$_dbpass = '' ;
-$_install_dir = '/var/virtual_www/htdocs/admin/restricted/facturation/' ;
-
-ini_set('include_path', ini_get('include_path').':'.$_install_dir.'PEAR:'.$_install_dir.'biblio') ;
-
-chdir($_SERVER['DOCUMENT_ROOT'].'/facturation/src/Model/common/') ;
-
-require_once('common.php5') ;
-
-chdir('../..') ;
-
-//require_once('testdroit.php') ;
+<?php
+require_once(dirname(__FILE__).'/../src/Model/common/common.php5');
 ?>
