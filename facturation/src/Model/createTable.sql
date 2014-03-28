6508a8ff04117b2ad30fe6cc7aa685454956e12d
Chapeau Gris <lionel@chapeau-gris.fr>
2014-03-28 16:29:06 +0100

    remove Hotel class and add Coworker class instead

--- facturation/src/Model/createTable.sql
+++ facturation/src/Model/createTable.sql
@@ -1,28 +1,20 @@
 
-create table HOTELs (
-	id_hotel INTEGER PRIMARY KEY 
+create table COWORKERs (
+	id_coworker INTEGER PRIMARY KEY 
 	,
-	factures  NOT NULL
+	nom_coworker VARCHAR(200) NOT NULL
 	,
-	nom_hotel VARCHAR(200) NOT NULL
+	adresse_coworker VARCHAR(200) NOT NULL
 	,
-	adresse_hotel VARCHAR(200) NOT NULL
+	code_postal_coworker VARCHAR(200) NOT NULL
 	,
-	code_postal_hotel VARCHAR(200) NOT NULL
+	ville_coworker VARCHAR(200) NOT NULL
 	,
-	ville_hotel VARCHAR(200) NOT NULL
+	email_coworker VARCHAR(200) NOT NULL
 	,
-	email_hotel VARCHAR(200) NOT NULL
-	,
-	web_hotel VARCHAR(200) NOT NULL
-	,
-	gerant_hotel VARCHAR(200) NOT NULL
-	,
-	contact_hotel VARCHAR(200) NOT NULL
+	nom_facture_coworker VARCHAR(200) NOT NULL
 	,
 	dt_install VARCHAR(200) NOT NULL
-	,
-	dt_trialend VARCHAR(200) NOT NULL
 	,
 	dt_end VARCHAR(200) NOT NULL
 	,
@@ -33,7 +25,7 @@ create table HOTELs (
 create table FACTUREs (
 	id_facture INTEGER PRIMARY KEY 
 	,
-	id_hotel	INTEGER NOT NULL
+	id_coworker	INTEGER NOT NULL
 	,
 	numero_facture VARCHAR(200) NOT NULL
 	,
@@ -43,15 +35,11 @@ create table FACTUREs (
 	,
 	url_pdf VARCHAR(200) NOT NULL
 	,
-	affice_tva BOOL NOT NULL
+	affiche_tva BOOL NOT NULL
 	,
 	montant_reduction VARCHAR(200) NOT NULL
 	,
 	libelle_reduction VARCHAR(200) NOT NULL
-	,
-	facture_option 
-,
- 	CONSTRAINT facture_option_UNIQUE UNIQUE(facture_option)
 );
 
 
@@ -75,15 +63,7 @@ create table FACTURE_OPTIONs (
 	,
 	id_option	INTEGER NOT NULL
 	,
-	option  NOT NULL
-,
- 	CONSTRAINT option_UNIQUE UNIQUE(option)
-	,
 	offert BOOL NOT NULL
 ,
  	CONSTRAINT offert_UNIQUE UNIQUE(offert)
 );
-
-
-
-
